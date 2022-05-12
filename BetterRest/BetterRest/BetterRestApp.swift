8a717ab1bcfd4d6d5115c99d6d9cd12bf7ea939e
Hemant Sharma <44858683+imhs7@users.noreply.github.com>
2022-05-12 08:29:25 +0530

    Add files via upload

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Hemant Sharma on 11/05/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
